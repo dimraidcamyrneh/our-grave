2ed92ee4288aaec0aa9a5d799404a96d58c457f9
dimraidcamyrneh <contact@henrymacdiarmid.com>
2022-06-22 19:28:38 -0700

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,8 @@ module.exports = {
     title: `Our Grave`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", 
+  /*{
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
@@ -17,5 +18,11 @@ module.exports = {
       "path": "./src/pages/"
     },
     __key: "pages"
-  }]
+  },*/ {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+}]
 };
